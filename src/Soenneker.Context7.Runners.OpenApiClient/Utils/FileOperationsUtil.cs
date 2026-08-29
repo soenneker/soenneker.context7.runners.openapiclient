ffd029eff31f9d2eae369e81ff844c88e0a1ee7d
soenneker <jake@soenneker.com>
2026-08-29 14:50:23 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Context7.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Context7.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -21,7 +21,7 @@ using System.Collections.Generic;
 
 namespace Soenneker.Context7.Runners.OpenApiClient.Utils;
 
-///<inheritdoc cref="IFileOperationsUtil"/>
+/// <inheritdoc cref="IFileOperationsUtil"/>
 public sealed class FileOperationsUtil : IFileOperationsUtil
 {
     private readonly ILogger<FileOperationsUtil> _logger;
